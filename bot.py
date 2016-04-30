b8b45de3687799580e0af8677631821561028fc4
David Case <Dav3xor@gmail.com>
2016-04-30 10:34:33 -0700

    added common word removal.

--- bot.py
+++ bot.py
@@ -9,7 +9,26 @@ from slackclient import SlackClient
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+common_words = ['the', 'be', 'to', 'of', 'and', 'a', 'in',
+                'that', 'have', 'I', 'it', 'for', 'not',
+                'on', 'with', 'he', 'as', 'you', 'do', 'at',
+                'this', 'but', 'his', 'by', 'from', 'they',
+                'we', 'say', 'her', 'she', 'or', 'an', 'will',
+                'my', 'one', 'all', 'would', 'there', 'their',
+                'what', 'so', 'up', 'out', 'if', 'about',
+                'who', 'get', 'which', 'go', 'me', 'when',
+                'make', 'can', 'like', 'time', 'no', 'just',
+                'him', 'know', 'take', 'person', 'into', 'year',
+                'your', 'good', 'some', 'could', 'them', 'see',
+                'other', 'than', 'then', 'now', 'look', 'only',
+                'come', 'its', 'over', 'think', 'also', 'back',
+                'after', 'use', 'two', 'how', 'our', 'work',
+                'first', 'well', 'way', 'even', 'new', 'want',
+                'because', 'any', 'these', 'give', 'day', 'most',
+                'us', 'are' 'aren\'t', 'haven\'t']
 
+common_words = set([i.upper() for i in common_words])
+print "common words = " + str(common_words)
 class LoudHailer(object):
   uppercase   = frozenset(string.uppercase)
   all_caps    = re.compile('^[A-Z\d\s\!\@\#\$\%\^\&\*'+
@@ -83,8 +102,12 @@ class LoudHailer(object):
     
     def do_loud_gif(user, domain, channel):
       loud = r.hget('loudlast', domain+'/'+channel)
-      if loud:
-        sample = loud.split(" ")[0:5]
+      if not loud:
+        return None
+      else:
+        words  = set(loud.split(" "))
+        sample = list(words.difference(common_words))[0:5]
+        print "sample = " + str(sample)
 
       url ='http://api.giphy.com/v1/gifs/search?'
 
